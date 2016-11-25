b0262e4c14557409b22bbf1ebc6d7232849d7b4f
Tomohiko Kinebuchi <tomohiko@Luna.local>
2016-11-25 10:59:43 +0900

    Create a framework core as ExtJS application
    .. code-block::
    
       sencha -sdk ~/local/extjs_sdk/ext-6.2.0 generate app Formis ./extjs/formis_core

---
+++ extjs/formis_core/build/temp/development/Formis/sass/config.rb
@@ -0,0 +1,2 @@
+Compass.add_project_configuration('../../../../../sass/config.rb')
+cache_path = '/Users/tomohiko/MyRepos/Java/formalistic-extjs/extjs/formis_core/build/.sass-cache'
---
+++ extjs/formis_core/build/temp/development/Formis/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require /Users/tomohiko/MyRepos/Java/formalistic-extjs/extjs/formis_core/app.js
