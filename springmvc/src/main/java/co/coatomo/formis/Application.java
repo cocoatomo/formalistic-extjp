ebe983230db1877cdc4ac447ff1b0320c041edb1
Tomohiko Kinebuchi <tomohiko@Luna.local>
2016-12-03 20:28:52 +0900

    Implement minimum example app

---
+++ springmvc/src/main/java/co/coatomo/formis/Application.java
@@ -0,0 +1,27 @@
+package co.coatomo.formis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class Application extends SpringBootServletInitializer {
+
+    @RequestMapping("/hello")
+    String home() {
+        return "Hello World!";
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
